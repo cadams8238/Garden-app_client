46d98ccca8fa140edda16ea92b464189e5ad96df
Courtney Adams <cadams8238@gmail.com>
2018-07-02 13:32:18 -0700

    added weatherData reducer and set up combineReducers for future reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import weatherDataReducer from './weatherData';
+
+const rootReducer = combineReducers({
+    weatherDataReducer
+})
+
+export default rootReducer;
---
+++ src/reducers/weatherDataReducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+    data: {},
+    loading: false,
+    error: null
+}
+
+const weatherDataReducer = (state = initialState, action) => {
+    if(action.type === 'FETCH_WEATHER_DATA_REQUEST') {
+        return {
+            ...state,
+            loading: true
+        }
+    }
+    else if(action.type === 'FETCH_WEATHER_DATA_SUCCESS') {
+        return {
+            ...state,
+            loading: false,
+            data: action.data
+        }
+    }
+    else if(action.type === 'FETCH_WEATHER_DATA_ERROR') {
+        return {
+            ...state,
+            loading: false,
+            error: action.err
+        }
+    }
+
+    return state;
+}
+
+export default weatherDataReducer;
