bbc9913ad86614e83d19c3ab02c1066ecf26c1a6
Courtney Adams <cadams8238@gmail.com>
2018-07-02 13:40:06 -0700

    added store.js and imported it into index.js to hook up the store to reducers/actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import weatherDataReducer from './weatherData';
+import weatherDataReducer from './weatherDataReducer';
 
 const rootReducer = combineReducers({
     weatherDataReducer
