6925e067001629de72533ae99d7c7547731266fc
Courtney Adams <cadams8238@gmail.com>
2018-07-03 08:39:49 -0700

    got some weather data from api to render in front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Form from './components/form-landingPage';
+import Test from './components/test';
 
 
 export default class App extends Component {
   render() {
     return(
         <div>
-            <Form />
+            {/* <Form /> */}
+            <Test />
         </div>
     );
   }
--- src/actions/weatherData.js
+++ src/actions/weatherData.js
@@ -3,21 +3,21 @@ import { normalizeResponseErrors } from './utils';
 
 
 export const FETCH_WEATHER_DATA_REQUEST = 'FETCH_WEATHER_DATA_REQUEST';
-const fetchWeatherDataRequest = () => {
+const fetchWeatherDataRequest = () => ({
     type: FETCH_WEATHER_DATA_REQUEST
-}
+})
 
 export const FETCH_WEATHER_DATA_SUCCESS = 'FETCH_WEATHER_DATA_SUCCESS';
-const fetchWeatherDataSuccess = data => {
+const fetchWeatherDataSuccess = data => ({
     type: FETCH_WEATHER_DATA_SUCCESS,
     data
-}
+})
 
 export const FETCH_WEATHER_DATA_ERROR = 'FETCH_WEATHER_DATA_ERROR';
-const fetchWeatherDataError = error => {
+const fetchWeatherDataError = error => ({
     type: FETCH_WEATHER_DATA_ERROR,
     error
-}
+})
 
 
 export const fetchWeatherData = () => dispatch => {
@@ -26,7 +26,7 @@ export const fetchWeatherData = () => dispatch => {
         fetch(`${API_BASE_URL}/weatherData`)
             .then(response => normalizeResponseErrors(response))
             .then(res => res.json())
-            .then(jsonData => dispatch(fetchWeatherDataSuccess(jsonData)))
+            .then(jsonData => dispatch(fetchWeatherDataSuccess(jsonData.list)))
             .catch(err => dispatch(fetchWeatherDataError(err)))
     );
 }
