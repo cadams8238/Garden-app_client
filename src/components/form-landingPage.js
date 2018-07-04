f8bf60c86bcc92b0db537aa48027c339ca67224b
Courtney Adams <cadams8238@gmail.com>
2018-07-04 15:31:30 -0700

    added user action for registering a new user

--- src/components/form-landingPage.js
+++ src/components/form-landingPage.js
@@ -4,24 +4,30 @@ import Form_signUp from './form-signUp';
 import Form_login from './form-login';
 import './styles/form-landingPage.css';
 
-export default function Form() {
-    return (
-        <Router>
-            <div>
-                <header>
-                    {/* add link here to landing page when completed */}
-                    <h1 className="logo">Phytochor</h1>
-                </header>
-                <main>
-                    {/* <img />
-                    <img /> */}
+export default class Form extends React.Component {
+    onSubmit(values) {
 
-                    {/* <Form_login /> */}
-                    {/* <Form_signUp /> */}
-                    <Route exact path="/signUp" component={Form_signUp} />
-                    <Route exact path="/login" component={Form_login} />
-                </main>
-            </div>
-        </Router>
-    );
+    }
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <header>
+                        {/* add link here to landing page when completed */}
+                        <h1 className="logo">Phytochor</h1>
+                    </header>
+                    <main>
+                        {/* <img />
+                        <img /> */}
+
+                        {/* <Form_login /> */}
+                        {/* <Form_signUp /> */}
+                        <Route exact path="/signUp" component={Form_signUp} />
+                        <Route exact path="/login" component={Form_login} />
+                    </main>
+                </div>
+            </Router>
+        );
+    }
 }
